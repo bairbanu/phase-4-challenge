dbf843bbc62f711d8087bdf8be5d2549c753aeb6
zubairnahmed <zubair.ahmed@alumni.stanford.edu>
2017-09-18 13:33:31 -0700

    add database middleware to handle login operations

--- src/models/users.js
+++ src/models/users.js
@@ -1,5 +1,5 @@
 const db = require('./database/queries/users');
-const { encryptPassword } = require('./helper-functions');
+const { encryptPassword, comparePassword, loginUser } = require('./helper-functions');
 
 function create(user) {
   const { name, email , password } = user;
@@ -17,8 +17,29 @@ function create(user) {
     });
 }
 
+function loginByEmail(user, req) {
+  const { email, password: plainPassword } = user;
+  let verifiedUser;
+
+  return db.getUserByEmail(email)
+    .then((user) => {
+      const { password: hash } = user;
+      verifiedUser = {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        date_joined: user.date_joined
+      };
+      return comparePassword(plainPassword, hash);
+    })
+    .then((match) => {
+      if (match) loginUser(verifiedUser, req);
+      else throw Error('incorrect email and/or password');
+    })
+}
+
 module.exports = {
   create,
   getById: db.getUserById,
-  getByEmail: db.getUserByEmail
+  loginByEmail
 };
