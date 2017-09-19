c3480be083e3cd996606448044cb3c39c84ee04d
zubairnahmed <zubair.ahmed@alumni.stanford.edu>
2017-09-19 16:00:17 -0700

    fix minor vbug with header

--- src/server/routes/albums.js
+++ src/server/routes/albums.js
@@ -3,20 +3,14 @@ const router = express.Router();
 
 const albums = require('../../models/albums');
 const reviews = require('../../models/reviews');
+const { isLoggedIn } = require('../middlewares/validation');
 
-// this is a partially authorized route -- have to add authorization check middleware
-// do this after setting up authentication
-router.get('/:albumID', (req, res) => {
+router.get('/:albumID', isLoggedIn, (req, res) => {
   const { albumID } = req.params;
 
   Promise.all([albums.getById(albumID), reviews.getByAlbumId(albumID)])
     .then(([album, reviews]) => {
-      // check logged in status before rendering proper page
-      // if logged in, render logged in 'true' header
-      // if logged in, show add review button and delete review icon
-
-      // the reviews here need user information
-      res.render('album', { album: album[0], reviews, loggedIn: false });
+      res.render('album', { album: album[0], reviews, loggedIn: req.isLoggedIn });
     })
     .catch((error) => {
       res.status(500).render('error', { error: error })
